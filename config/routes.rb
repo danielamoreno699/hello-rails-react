9c84be2c2046e6c88f6e6a074a2cc8bae18a55f6
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 10:35:40 -0500

    set up the route for the api on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :messages, only: [:index]
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index]
+    end
+  end
   
   
   root 'root#index'
