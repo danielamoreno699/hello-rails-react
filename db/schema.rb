1a65795e7e9ccccc3f048c38f15a6ea9d86ffe97
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 10:36:19 -0500

    created migration for table

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_001500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # create_table "messages", force: :cascade do |t|
-  #   t.string "greeting"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
+  create_table "messages", force: :cascade do |t|
+    t.string "greeting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
