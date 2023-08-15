6cbf631d2912ebe176d740292d9fd3f9b256ce73
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 10:37:21 -0500

    set up application js with routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,25 +1,25 @@
-// import "@hotwired/turbo-rails";
-// import "./controllers";
+import "@hotwired/turbo-rails";
+import "./controllers";
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Routes } from 'react-router-dom';
 
-// import { Message } from "./components/Message";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { Provider } from 'react-redux';
-// import store from "./redux/store";
+import { Message } from "./components/Message";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./redux/store";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<Message />} />
-//       </Routes>
-//     </Router>
-//   </Provider>
-// </React.StrictMode>,
-// );
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Message />} />
+      </Routes>
+    </Router>
+  </Provider>
+</React.StrictMode>,
+);
