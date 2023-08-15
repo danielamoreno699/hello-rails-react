f41e536752a9f58bf89d7232dabaf273a3519d15
Daniela Moreno <dani9292@hotmail.com>
2023-08-15 10:36:41 -0500

    created a message react component

--- app/javascript/components/Message.js
+++ app/javascript/components/Message.js
@@ -1,21 +1,21 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getMessages } from '../redux/messagesSlice';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMessages } from '../redux/messagesSlice';
 
 
-// export const Message = () => {
-//   const dispatch = useDispatch();
-//   const messages = useSelector((state) => state.messages.messages);
+export const Message = () => {
+  const dispatch = useDispatch();
+  const messages = useSelector((state) => state.messages.messages);
 
-//   useEffect(() => {
-//     dispatch(getMessages());
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(getMessages());
+  }, [dispatch]);
 
-//   console.log(messages)
-//   return (
-//     <div>
-//       <h2>Messages:</h2>
-//       {messages.greeting}
-//     </div>
-//   );
-// };
+  console.log(messages)
+  return (
+    <div>
+      <h2>Messages:</h2>
+      {messages.greeting}
+    </div>
+  );
+};
